b8481938ccb4061ab0bafb0e58e187435f2cf16b
Harold_Mesa <harold.mesa@icloud.com>
2022-09-23 18:05:15 -0500

    WIP: Working on Business Logic for 5th TDD for Bread class.

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -16,14 +16,15 @@ namespace PierresBakery
     {
       int price = 5;
 
-      if (Quantity == 2)
+      if(Quantity == 2)
       {
         price = price * 2;
-      } 
-      else 
-      {
-      return price;
       }
+      else
+      {
+      // breadCost = (((Bread - (Bread % 3)) * 5) * 2/3) + ((Bread % 3) * 5);
+      price = (((Quantity - (Quantity % 3)) * 5) * 2/3) + ((Quantity %3) * 5);
+      } 
       return price;
     }
 
