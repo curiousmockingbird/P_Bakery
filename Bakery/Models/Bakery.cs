06d5b7532e79d6776881a49ffc77c975a6427177
Harold_Mesa <harold.mesa@icloud.com>
2022-09-24 00:22:04 -0500

    Pass 5th TDD for Pastry class.

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -33,9 +33,8 @@ namespace PierresBakery
         Q = 3 -> p =(p * Q) - p * 1       Q = 6 -> p = (p * Q) - p * 2      Q = 9 -> p = (p * Q) - p * 3
         */  
       
-          price = (price * Quantity) - (price * (Quantity % 3 - ((Quantity % 3) - (Quantity / 3))));
-        
-                                
+        price = (price * Quantity) - (price * (Quantity % 3 - ((Quantity % 3) - (Quantity / 3))));
+
       } 
       return price;
     }
@@ -57,13 +56,22 @@ namespace PierresBakery
     public int CalcPastryOrderPrice()
     {
       int price = 2;
+
+      if (Quantity == 1)
+      {
+        price = 2;
+      }
       
-      if (Quantity == 2)
+      else if (Quantity == 2)
       {
        price = Quantity * 2;
-      } else 
+      } 
+
+      else 
       {
-        
+        // price = (price * Quantity) - (Quantity / 2);
+        price = (price * Quantity) - (price * 1/2);
+
       }
 
       return price;
